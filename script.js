7d3e84d32b493a590b215754c82422c07fe8140a
BenihiroDev <aruludhish1717@gmail.com>
2026-03-18 07:40:15 +0530

    pads pop-up

--- script.js
+++ script.js
@@ -1,8 +1,11 @@
 const powerButton = document.getElementById("power-button");
 const beep = new Audio("audio/beep.mp3");
 const padConnector = document.getElementById("pad-connector");
+const padPopup = document.getElementById("pad-popup");
+const closePopup = document.getElementById("close-popup");
 
 let powerOn = false;
+let pressTimer;
 
 powerButton.addEventListener("click", function() {
   powerOn = !powerOn;
@@ -14,6 +17,32 @@ powerButton.addEventListener("click", function() {
   } else {
     powerButton.classList.remove("on");
     padConnector.classList.remove("active");
-
   }
+});
+
+
+
+function startPress() {
+  if (powerOn) {
+    pressTimer = setTimeout(function() {
+      padPopup.classList.remove("hidden");
+    }, 1000);
+  }
+}
+
+function cancelPress() {
+  clearTimeout(pressTimer);
+}
+
+padConnector.addEventListener("mousedown", startPress);
+padConnector.addEventListener("mouseup", cancelPress);
+padConnector.addEventListener("mouseleave", cancelPress);
+
+padConnector.addEventListener("touchstart", startPress);
+padConnector.addEventListener("touchend", cancelPress);
+
+
+
+closePopup.addEventListener("click", function() {
+  padPopup.classList.add("hidden");
 });
